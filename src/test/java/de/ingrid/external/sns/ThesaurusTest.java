02b2400406bb5dceb9dc5f6e1d54876556de2842
André Wallat <andre.wallat@wemove.com>
2022-01-07 15:18:02 +0100

    Adapt test to changed external result

--- src/test/java/de/ingrid/external/sns/ThesaurusTest.java
+++ src/test/java/de/ingrid/external/sns/ThesaurusTest.java
@@ -381,7 +381,7 @@ public class ThesaurusTest extends TestCase {
 		TreeTerm startTerm = thesaurusService.getHierarchyPathToTop(termId, locale);
 		// start term is term with requested id
 		assertEquals(termId, startTerm.getId());
-        String[] expectedPath = {"Spreewald", "[Deutsche Biosphärenreservate]", "Bundesrepublik Deutschland", "Mitteleuropa", "Europa"};
+        String[] expectedPath = {"Spreewald", "[Deutsche Biosphärenreservate]", "[Biosphärenreservate]", "Großschutzgebiet"};
 		assertHierarchyPath( expectedPath, startTerm );
 	}
 	
